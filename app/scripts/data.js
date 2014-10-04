afc0327af604ec514489594b7f08fa6920a7aa3c
Jay Wilson Jr <info@jaywilsonjr.com>
2014-10-04 17:12:25 -0400

    Added itemView.

---
+++ app/scripts/data.js
@@ -0,0 +1,17 @@
+var items = [
+{
+  title: 'Pad Thai',
+  desc: 'Thin rice noodle stir-fried with meat or tofu, egg, green onion, chopped red onion, chopped tofu and bean sprouts in Pad Thai sauce topped with chopped peanuts.',
+  price: '$13.00', 
+  img_url: '../images/padThai.jpg', 
+  cat: 'noodles' 
+},
+
+{
+  title: 'Pad See Eew',
+  desc: 'Wide rice noodle stir-fried with meat and tofu,egg and broccoli in a sweet Thai brown sauce.',
+  price: '$12.00',
+  img_url: '../padSeeEew.jpg',
+  cat: 'noodles'
+}
+]
