08b3c60c1d67f073d670821f03a5de21a607dc5f
Jay Wilson Jr <info@jaywilsonjr.com>
2014-10-05 22:52:48 -0400

    Added menuView and related child views

--- app/scripts/data.js
+++ app/scripts/data.js
@@ -1,17 +1,33 @@
 var items = [
 {
-  title: 'Pad Thai',
-  desc: 'Thin rice noodle stir-fried with meat or tofu, egg, green onion, chopped red onion, chopped tofu and bean sprouts in Pad Thai sauce topped with chopped peanuts.',
-  price: '$13.00', 
+  title:   'Pad Thai',
+  desc:    'Thin rice noodle stir-fried with meat or tofu, egg, green onion, chopped red onion, chopped tofu and bean sprouts in Pad Thai sauce topped with chopped peanuts.',
+  price:   '$13.00', 
   img_url: '../images/padThai.jpg', 
-  cat: 'noodles' 
+  cat:     'noodles' 
 },
 
 {
-  title: 'Pad See Eew',
-  desc: 'Wide rice noodle stir-fried with meat and tofu,egg and broccoli in a sweet Thai brown sauce.',
-  price: '$12.00',
-  img_url: '../padSeeEew.jpg',
-  cat: 'noodles'
+  title:   'Pad See Eew',
+  desc:    'Wide rice noodle stir-fried with meat and tofu,egg and broccoli in a sweet Thai brown sauce.',
+  price:   '$12.00',
+  img_url: '../images/padSeeEew.jpg',
+  cat:     'noodles'
+}, 
+
+{
+  title:   'Drunken Noodle', 
+  desc:    'Wide rice noodle stir-fried with fresh chili, Thai basil, onion and bell pepper in a spicy Thai sauce. Hot',
+  price:   '$11.00',
+  img_url: '../images/drunkenNoodle.jpg',
+  cat:     'noodles'
+}, 
+
+{
+  title:   'Thai Chef’s Noodle', 
+  desc:    'Wide rice noodle stir-fried with meat or tofu, onion, bell pepper, zucchini,broccoli and baby corn in a spicy Thai chili sauce (Thai chili sauce contains shrimp). Hot', 
+  price:   '$14.00', 
+  img_url: '../images/thaiNoodle.jpg',
+  cat:     'noodles'
 }
 ]
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,36 +4,69 @@
 /* ==========================================================================
    VIEWS
    ========================================================================== */
-    var ItemView = Backbone.View.extend({
-      className: 'menu_item', 
+    var MenuView = Backbone.View.extend({
+      className: 'menu_container', 
 
-      render: function() {
-        $('body').append(this.el);
+      render: function(options) {
+        $('.container').append(this.el);
+        this.collection.each(_.bind(this.renderChild, this))
+      },
+
+      renderChild: function(dishModel) {
+        new SingleDishView({model: dishModel})
+      },
+
+      initialize: function(options) {
+        this.render(options);
       }
+    });
+
+    
+    var SingleDishView = Backbone.View.extend({
+      className: 'menu_item',
+
+      template: _.template($('#template_single_dish').text()),
+
+      initialize: function() {
+        this.$el.html(this.template(this.model.attributes));
+        console.log(this.$el);
+        $('.menu_container').append(this.el);
+      }
+
     });
 
 /* ==========================================================================
    MODELS
    ========================================================================== */
     var DishModel = Backbone.Model.extend({
-      
+
     });
 
 /* ==========================================================================
    COLLECTIONS
    ========================================================================== */
+    var DishCollection = Backbone.Collection.extend({
+      model: DishModel
 
+    });
 
 /* ==========================================================================
    GLUE
    ========================================================================== */
-
-
   $(document).ready(function() {
-    var itemView = new ItemView();
-    itemView.render();
+    var dishCollection = new DishCollection();
+    dishCollection.set(items);
 
-    var dishModel = new DishModel();
+    var menuView = new MenuView({collection: dishCollection});
+    
+    var singleDishView = new SingleDishView();
+    singleDishView.render();
+
+    // var dishModel = new DishModel();
+    // dishModel.set({});
+
+    
+
 
     $('.menu_item').on('click', function() {
       $(this).toggleClass('visible');
